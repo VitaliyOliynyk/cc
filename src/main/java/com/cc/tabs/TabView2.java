7216d45848c6d3045bfbe4837c3e4defe6a44d1e
VitaliyOliynyk <xaoc00@gmail.com>
2014-10-27 23:11:14 +0100

    Zagnieżdżone elementy z beans:bean

---
+++ src/main/java/com/cc/tabs/TabView2.java
@@ -0,0 +1,14 @@
+package com.cc.tabs;
+
+public class TabView2 extends TabView{
+
+    String customProperty;
+
+    public String getCustomProperty() {
+        return customProperty;
+    }
+
+    public void setCustomProperty(String customProperty) {
+        this.customProperty = customProperty;
+    }
+}
